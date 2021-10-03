62c02202380411a60efd6400bf7227b3ee56714b
Shubham Roy <36968449+ShubhamRoy96@users.noreply.github.com>
2021-10-03 15:12:38 +0530

    Added new functionality in settings

--- APControlCentre/View/Settings.Designer.cs
+++ APControlCentre/View/Settings.Designer.cs
@@ -30,6 +30,11 @@
         {
             this.btnConnect = new System.Windows.Forms.Button();
             this.cboPorts = new System.Windows.Forms.ComboBox();
+            this.txtRed = new System.Windows.Forms.TextBox();
+            this.txtBlue = new System.Windows.Forms.TextBox();
+            this.txtGreen = new System.Windows.Forms.TextBox();
+            this.btnApply = new System.Windows.Forms.Button();
+            this.btnPowerON = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnConnect
@@ -55,16 +60,67 @@
             this.cboPorts.Size = new System.Drawing.Size(121, 24);
             this.cboPorts.TabIndex = 2;
             // 
+            // txtRed
+            // 
+            this.txtRed.Location = new System.Drawing.Point(708, 111);
+            this.txtRed.Name = "txtRed";
+            this.txtRed.Size = new System.Drawing.Size(100, 20);
+            this.txtRed.TabIndex = 4;
+            // 
+            // txtBlue
+            // 
+            this.txtBlue.Location = new System.Drawing.Point(708, 163);
+            this.txtBlue.Name = "txtBlue";
+            this.txtBlue.Size = new System.Drawing.Size(100, 20);
+            this.txtBlue.TabIndex = 5;
+            // 
+            // txtGreen
+            // 
+            this.txtGreen.Location = new System.Drawing.Point(708, 137);
+            this.txtGreen.Name = "txtGreen";
+            this.txtGreen.Size = new System.Drawing.Size(100, 20);
+            this.txtGreen.TabIndex = 6;
+            // 
+            // btnApply
+            // 
+            this.btnApply.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btnApply.Font = new System.Drawing.Font("Overpass", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnApply.Location = new System.Drawing.Point(727, 189);
+            this.btnApply.Name = "btnApply";
+            this.btnApply.Size = new System.Drawing.Size(75, 23);
+            this.btnApply.TabIndex = 7;
+            this.btnApply.Text = "Apply";
+            this.btnApply.UseVisualStyleBackColor = true;
+            this.btnApply.Click += new System.EventHandler(this.btnApply_Click);
+            // 
+            // btnPowerON
+            // 
+            this.btnPowerON.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btnPowerON.Font = new System.Drawing.Font("Overpass", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPowerON.Location = new System.Drawing.Point(727, 232);
+            this.btnPowerON.Name = "btnPowerON";
+            this.btnPowerON.Size = new System.Drawing.Size(75, 23);
+            this.btnPowerON.TabIndex = 8;
+            this.btnPowerON.Text = "ON";
+            this.btnPowerON.UseVisualStyleBackColor = true;
+            this.btnPowerON.Click += new System.EventHandler(this.btnPowerON_Click);
+            // 
             // Settings
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnPowerON);
+            this.Controls.Add(this.btnApply);
+            this.Controls.Add(this.txtGreen);
+            this.Controls.Add(this.txtBlue);
+            this.Controls.Add(this.txtRed);
             this.Controls.Add(this.btnConnect);
             this.Controls.Add(this.cboPorts);
             this.Name = "Settings";
             this.Size = new System.Drawing.Size(832, 468);
             this.Load += new System.EventHandler(this.Settings_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -72,5 +128,10 @@
 
         private System.Windows.Forms.Button btnConnect;
         private System.Windows.Forms.ComboBox cboPorts;
+        private System.Windows.Forms.TextBox txtRed;
+        private System.Windows.Forms.TextBox txtBlue;
+        private System.Windows.Forms.TextBox txtGreen;
+        private System.Windows.Forms.Button btnApply;
+        private System.Windows.Forms.Button btnPowerON;
     }
 }
--- APControlCentre/View/Settings.cs
+++ APControlCentre/View/Settings.cs
@@ -18,6 +18,7 @@ namespace APControlCentre.View
 
         String[] ports;
         DataSender dataSender;
+        public bool areLightsON { get; set; }
 
 
         public Settings()
@@ -89,6 +90,35 @@ namespace APControlCentre.View
 
                 throw;
             }
+        }
+
+        private void btnApply_Click(object sender, EventArgs e)
+        {
+            string RED = Math.Abs(Convert.ToInt32(txtRed.Text.PadLeft(3, '0')) - 255).ToString().PadLeft(3, '0');
+            string GREEN = Math.Abs(Convert.ToInt32(txtGreen.Text.PadLeft(3, '0')) - 255).ToString().PadLeft(3, '0');
+            string BLUE = Math.Abs(Convert.ToInt32(txtBlue.Text.PadLeft(3, '0')) - 255).ToString().PadLeft(3, '0');
+
+
+            string RGBdata = "#RGB" + RED + GREEN + BLUE + "|";
+            DataSender.Enqueue(RGBdata);
+        }
+
+        private void btnPowerON_Click(object sender, EventArgs e)
+        {
+            string lightsStatus = string.Empty;
+            if (!areLightsON)
+            {
+                areLightsON = true;
+                btnPowerON.Text = "OFF";
+                lightsStatus = "#STA|";
+            }
+            else
+            {
+                areLightsON = false;
+                btnPowerON.Text = "ON";
+                lightsStatus = "#STO|";
+            }            
+             DataSender.Enqueue(lightsStatus);
         }
     }
 }
